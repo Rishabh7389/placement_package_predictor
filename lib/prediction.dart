7467f0a3ac71d398f4ad9a5513ce18082dc13ffd
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2023-10-07 00:50:54 +0530

    changes in checkbox

--- lib/prediction.dart
+++ lib/prediction.dart
@@ -1,49 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:placement_package_predictor/result.dart';
 
 void main() {
   runApp(MaterialApp(
     home: PredictionPage(),
   ));
-}
-
-class CircularCheckBox extends StatelessWidget {
-  final bool value;
-  final ValueChanged<bool?> onChanged;
-
-  const CircularCheckBox({
-    required this.value,
-    required this.onChanged,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onChanged(!value);
-      },
-      child: Container(
-        width: 24,
-        height: 24,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(
-              color: value ? Colors.white : Colors.black,
-              width: 2), // Change border color based on the checkbox state
-          color: value ? Color(0x87A850) : Colors.white,
-        ),
-        child: Center(
-          child: value
-              ? Icon(
-                  Icons.check,
-                  size: 16,
-                  color: Colors.white,
-                )
-              : null,
-        ),
-      ),
-    );
-  }
 }
 
 class PredictionPage extends StatefulWidget {
@@ -66,7 +26,7 @@ class _PredictionPageState extends State<PredictionPage> {
             const Text(
               "SELECT YOUR SKILL",
               style: TextStyle(
-                fontSize: 54,
+                fontSize: 40,
                 fontWeight: FontWeight.w700,
                 color: Color(0xFF26206F),
               ),
@@ -75,50 +35,48 @@ class _PredictionPageState extends State<PredictionPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                _buildSkillContainer("AI/ML", 0),
-                _buildSkillContainer("PYTHON", 1),
-                _buildSkillContainer("JAVA", 2),
-                _buildSkillContainer("DSA", 3),
+                _buildSkillContainer("AI/ML      ", 0),
+                _buildSkillContainer("PYTHON      ", 1),
+                _buildSkillContainer("JAVA      ", 2),
+                _buildSkillContainer("DSA      ", 3),
               ],
             ),
             const SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                _buildSkillContainer("SQL", 4),
-                _buildSkillContainer("HTML", 5),
-                _buildSkillContainer("DEVOPS", 6),
-                _buildSkillContainer("POWER BI", 7),
+                _buildSkillContainer("SQL      ", 4),
+                _buildSkillContainer("HTML      ", 5),
+                _buildSkillContainer("DEVOPS      ", 6),
+                _buildSkillContainer("POWER BI      ", 7),
               ],
             ),
             const SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                _buildSkillContainer("FIGMA", 8),
-                _buildSkillContainer("REACT JS", 9),
-                _buildSkillContainer("NODE JS", 10),
-                _buildSkillContainer("CPP", 11),
+                _buildSkillContainer("FIGMA      ", 8),
+                _buildSkillContainer("REACT JS      ", 9),
+                _buildSkillContainer("NODE JS      ", 10),
+                _buildSkillContainer("CPP      ", 11),
               ],
             ),
             const SizedBox(height: 40),
             ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ResultPage()),
-                );
+                // Handle button press
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF26206F),
                 fixedSize: const Size(200, 60),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(19),
+                  borderRadius:
+                      BorderRadius.circular(19), // Set the border radius here
                 ),
               ),
               child: const Text(
                 "PREDICT NOW!",
-                style: TextStyle(fontSize: 18),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
             ),
           ],
@@ -130,44 +88,64 @@ class _PredictionPageState extends State<PredictionPage> {
   Widget _buildSkillContainer(String skillName, int index) {
     Color containerColor = Colors.white;
     Color textColor = Colors.black;
+    Color checkboxColor =
+        Colors.white; // Set checkbox color to match the container background
 
     if (isCheckedList[index]) {
       containerColor = Color(
           0xFF87A850); // Change container color to green when checkbox is selected
       textColor =
           Colors.white; // Change text color to white when checkbox is selected
+      checkboxColor =
+          Color(0xFF87A850); // Set checkbox color to match the selected state
     }
 
-    return Container(
-      margin: const EdgeInsets.symmetric(
-          horizontal: 8, vertical: 4), // Add margin for spacing
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: containerColor,
-        borderRadius: BorderRadius.circular(32),
-        border: Border.all(color: const Color(0xFF87A850), width: 2),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          CircularCheckBox(
-            value: isCheckedList[index],
-            onChanged: (bool? value) {
-              setState(() {
-                isCheckedList[index] = value ?? false;
-              });
-            },
-          ),
-          const SizedBox(width: 8),
-          Text(
-            skillName,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: textColor,
+    return InkWell(
+      onTap: () {
+        setState(() {
+          isCheckedList[index] = !isCheckedList[index]; // Toggle checkbox state
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: containerColor,
+          borderRadius: BorderRadius.circular(32),
+          border: Border.all(color: const Color(0xFF87A850), width: 2),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Invisible checkbox with the same background color as the container
+            Container(
+              width: 24,
+              height: 24,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: checkboxColor,
+                border: Border.all(
+                    color: Colors.transparent), // Hide the checkbox border
+              ),
+              child: isCheckedList[index] // Display checkmark when selected
+                  ? Icon(
+                      Icons.check_circle_rounded,
+                      size: 25,
+                      color: Color.fromRGBO(255, 253, 253, 1),
+                    )
+                  : null,
             ),
-          ),
-        ],
+            const SizedBox(width: 8),
+            Text(
+              skillName,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: textColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
