83b08b0431f6ca6fd4832099aec428a67a5f617a
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2023-10-06 09:34:56 +0530

    first commit

---
+++ lib/prediction.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class PredictionPage extends StatelessWidget {
+  const PredictionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              "SELECT YOUR SKILL",
+              style: TextStyle(
+                  fontFamily: 'poppins',
+                  fontSize: 54,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF26206F)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
